8ed767e5453da972008d9d92ecc8f83907c3bd31
saidabbos <athamovsaidabbos@gmail.com>
2025-11-14 23:25:28 +0500

    first commit

---
+++ lib/models/expense.dart
@@ -0,0 +1,35 @@
+import 'package:uuid/uuid.dart';
+import 'package:flutter/material.dart';
+import "package:intl/intl.dart";
+
+const uuid = Uuid();
+
+enum Category { food, travel, leisure, work }
+
+const CategoryIcons = {
+  Category.food: Icons.restaurant,
+  Category.travel: Icons.flight,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
+
+final formatter = DateFormat.yMd();
+
+class Expense {
+  Expense({
+    required this.title,
+    required this.amount,
+    required this.date,
+    required this.category,
+  }) : id = uuid.v4();
+
+  final String id;
+  final String title;
+  final double amount;
+  final DateTime date;
+  final Category category;
+
+  get formattedDate {
+    return formatter.format(date);
+  }
+}
---
+++ lib/widgets/expenses_list/new_expense.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class NewExpense extends StatefulWidget {
+  @override
+  State<NewExpense> createState() {
+    return _NewExpenseState();
+  }
+}
+
+class _NewExpenseState extends State<NewExpense> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.all(16),
+      child: Column(
+        children: const [
+          TextField(
+            maxLength: 50,
+            keyboardType: TextInputType.name,
+            decoration: InputDecoration(label: Text("Title")),
+          ),
+        ],
+      ),
+    );
+  }
+}
