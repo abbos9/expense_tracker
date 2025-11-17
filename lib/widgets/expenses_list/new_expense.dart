4189e252cf6053d067fff350111fca854cbddec2
saidabbos <athamovsaidabbos@gmail.com>
2025-11-17 21:25:30 +0500

    adding amount of money spent

--- lib/widgets/expenses_list/new_expense.dart
+++ lib/widgets/expenses_list/new_expense.dart
@@ -8,16 +8,40 @@ class NewExpense extends StatefulWidget {
 }
 
 class _NewExpenseState extends State<NewExpense> {
+  final _titleController = TextEditingController();
+  final _moneyController = TextEditingController();
+  
+  @override
+  void dispose() {
+    _titleController.dispose();
+    _moneyController.dispose();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: EdgeInsets.all(16),
       child: Column(
-        children: const [
+        children: [
           TextField(
+            controller: _titleController,
             maxLength: 50,
             keyboardType: TextInputType.name,
             decoration: InputDecoration(label: Text("Title")),
+          ),
+          TextField(
+            controller: _moneyController,
+            keyboardType: TextInputType.number,
+            decoration: InputDecoration(prefixText: "\$",label: Text("Amount Money Spent")),
+          ),
+          Row(
+            children: [
+              TextButton(onPressed: (){
+                Navigator.pop(context);
+              }, child: const Text("Cancel")),
+              ElevatedButton(onPressed: (){ print("\n ${_moneyController.text} was spent on ${_titleController.text}");}, child: Text("Save Expense"))
+            ],
           ),
         ],
       ),
