d542690f48ecc024aedc35c8e5a4ecd3986251d9
saidabbos <athamovsaidabbos@gmail.com>
2025-12-01 23:33:59 +0500

    removable expenses

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -42,8 +42,41 @@ class _ExpensesState extends State<Expenses> {
     });
   }
 
+  void removeExpense(Expense expense) {
+    final expenseIndex = _registeredExpenses.indexOf(expense);
+    setState(() {
+      _registeredExpenses.remove(expense);
+    });
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(
+      context,
+    ).showSnackBar(
+      SnackBar(
+        duration: Duration(seconds: 3),
+        content: Text('${expense.title} deleted.'),
+        action: SnackBarAction(
+          label: "Undo",
+          onPressed: () {
+            setState(() {
+              _registeredExpenses.insert(expenseIndex, expense);
+            });
+          },
+        ),
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
+    Widget mainContent = const Center(
+      child: Text("No expenses found. Start adding some!"),
+    );
+    if (_registeredExpenses.isNotEmpty) {
+      mainContent = ExpensesList(
+        expenses: _registeredExpenses,
+        onRemoveExpense: removeExpense,
+      );
+    }
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter Expense Tracker"),
@@ -57,7 +90,9 @@ class _ExpensesState extends State<Expenses> {
       body: Column(
         children: [
           Text("CHART!"),
-          Expanded(child: ExpensesList(expenses: _registeredExpenses)),
+          Expanded(
+            child: mainContent,
+          ),
         ],
       ),
     );
