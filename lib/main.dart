4a52dccc02eb30880dc7fe3338565733d6096b39
saidabbos <athamovsaidabbos@gmail.com>
2025-12-02 22:58:02 +0500

    adding new feautures such as Theme

--- lib/main.dart
+++ lib/main.dart
@@ -2,10 +2,38 @@ import "package:flutter/material.dart";
 
 import "package:expense_tracker/widgets/expenses.dart";
 
+var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.deepPurple);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+        ),
+        cardTheme: CardThemeData().copyWith(
+          color: kColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kColorScheme.primaryContainer,
+            foregroundColor: kColorScheme.onPrimaryContainer,
+          ),
+        ),
+        textTheme: ThemeData().textTheme.copyWith(
+          titleLarge: TextStyle(
+            fontWeight: FontWeight.normal,
+            color: kColorScheme.onSecondaryContainer,
+            fontSize: 14,
+          ),
+        ),
+      ),
       home: Expenses(),
     ),
   );
