6d68e0b5ec4a81f8fab857519535649add5d8cea
saidabbos <athamovsaidabbos@gmail.com>
2025-12-03 14:11:45 +0500

    module finished

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,29 @@ import "package:expense_tracker/widgets/expenses.dart";
 
 var kColorScheme = ColorScheme.fromSeed(seedColor: Colors.deepPurple);
 
+var kDarkColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 47, 25, 86),
+  brightness: Brightness.dark,
+);
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        cardTheme: CardThemeData().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: EdgeInsets.symmetric(
+            horizontal: 16,
+            vertical: 8,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimaryContainer,
+          ),
+        ),
+      ),
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: AppBarTheme().copyWith(
@@ -34,6 +54,7 @@ void main() {
           ),
         ),
       ),
+      themeMode: ThemeMode.system,
       home: Expenses(),
     ),
   );
